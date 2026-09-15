0aeabbf70f82a099b07847e0e5a9bb5a23144467
Пуфыстык <@DESKTOP-1ATV8SD>
2026-09-16 00:47:09 +0300

    Реализовала функцию подсчёта площади комнаты, с повторным запросом данных если площадь комнаты меньше или равна 0

---
+++ Лаб1/ЛБ 1/ЛБ 1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ЛБ_1
+{
+    internal class Program
+    {
+        public static double CalculateSquare()
+        {
+            Console.WriteLine("Введите стороны комнаты");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            double c = Convert.ToDouble(Console.ReadLine());
+            double Square = a * b * c; //square - эт площадь
+            if (Square <= 0)
+            {
+                Console.WriteLine("Ошибка, площадь составила <= 0"); //ты серьезно думал что этог сработает...?
+                CalculateSquare();
+            }
+            Console.WriteLine("Площадь стен: " + Square);
+            return Square;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
